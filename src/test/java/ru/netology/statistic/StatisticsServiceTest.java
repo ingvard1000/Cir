994f66619762320fac2acbc8c2a55aab265dfdba
Ingvard <ingvard777333@gmail.com>
2026-02-16 15:24:26 +0300

    fullTestCoverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,12 +6,30 @@ public class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void secondBranchtMax() {
+        StatisticsService service = new StatisticsService();
+        long[] arr = {-2, -15, -18};
+        long expected = -2;
+        long actual = service.findMax(arr);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void assignmentNewMax() {
+        StatisticsService service = new StatisticsService();
+        long[] arr = {2, 15, 18};
+        long expected = 18;
+        long actual = service.findMax(arr);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
 }
